235fee961ed76f95764f8c1ea19cbe4ab6ac5d5b
binalbodar <binalbodar23@gmail.com>
2022-04-26 22:18:55 -0700

    hello

--- binal/src/App.js
+++ binal/src/App.js
@@ -2,6 +2,48 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  // var x=10;
+  // {
+  //   var x=20;
+  // }
+  // console.log(x);
+
+  // let x=10;
+  // {
+  //   let x=20;
+  // }
+  // console.log(x);
+
+  // const x=10;
+  // {
+  //   const x=20;
+  // }
+  // console.log(x);
+
+  // let y=5;
+  // y= y + 2;
+  // console.log(y);
+
+  // const z=5;
+  // z= z + 2;
+  // console.log(z);
+
+  // const a;
+  // a=50;
+  // console.log(a);
+
+  // let b;
+  // b=50;
+  // console.log(b);
+
+  // let arr =[10,20,30,40,50];
+  // arr.map((a ,i) => console.log(a));
+
+  let intro = () =>{
+    console.log("binal bodar")
+  }
+  intro();
   return (
     <div className="App">
       <header className="App-header">
