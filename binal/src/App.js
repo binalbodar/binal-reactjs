c6f046183c25c50340ebfedb08c2acf13182f604
binalbodar <binalbodar23@gmail.com>
2022-05-16 21:40:07 -0700

    16_5counter

--- binal/src/App.js
+++ binal/src/App.js
@@ -7,37 +7,12 @@ import Cityfun from './Container/Country/Cityfun';
 import Branch from './Container/Country/Branch';
 import Time from './Container/Country/Time/Time';
 import TimeFun from './Container/Country/Time/TimeFun';
-import { useEffect, useState } from 'react';
-import Loading from './Componats/Loading/Loading';
-import Home from './Container/Country/Home/Home';
-
-const HomeWithLoding=Loading(Home)
-
-function App() {
-  const [loading, setLoading]=useState(false);
-  const [data, setData]=useState([]);
-
-  let orgData=[
-    {id: 101, name:"Mansi"},
-    {id: 102, name:"Krinali"},
-    {id: 103, name:"Jignasa"}
-  ]
-
-  useEffect(
-    ()=>{
-      setLoading(true);
-      setTimeout(()=>{setLoading(false);setData(orgData)},2000);
-    },
-  [])
-console.log(loading, data);
-  return (
-    <>
-    <HomeWithLoding
-      isLoading={loading}
-      data={data}
-    />
-    </>
-  );
+import Counter from './Container/Country/Counter';
+function App(){
+    return(
+        <>
+            <Counter/>
+        </>
+    )
 }
-
 export default App;
