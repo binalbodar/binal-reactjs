d8a4f8397f067f8ab30f0f9574ccf975a6be9900
binalbodar <binalbodar23@gmail.com>
2022-05-16 22:42:10 -0700

    Hoc

--- binal/src/App.js
+++ binal/src/App.js
@@ -8,10 +8,33 @@ import Branch from './Container/Country/Branch';
 import Time from './Container/Country/Time/Time';
 import TimeFun from './Container/Country/Time/TimeFun';
 import Counter from './Container/Country/Counter';
+import { useEffect, useState } from 'react';
+import Loading from './Componats/Loading/Loading';
+import Home from './Container/Country/Home/Home';
+const HomewithLoading=Loading(Home)
 function App(){
+const [loading,setLoading]=useState(false);
+const [data,setData]=useState([]);
+
+let orgData =[
+  {id:100,name:"Krinali"},
+  {id:101,name:"Jigu"},
+  {id:101,name:"Mansi"},
+
+]
+useEffect(
+  ()=>{
+   setLoading(true);
+   setTimeout(()=>{setLoading(false);setData(orgData)},2000);
+  },
+  [])
+console.log(loading,data);
     return(
         <>
-            <Counter/>
+            <HomewithLoading
+ isloading={loading}
+ data={data}
+ />
         </>
     )
 }
