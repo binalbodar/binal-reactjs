ee44bfc35c3fde511c334acbd269467450849764
binalbodar <binalbodar23@gmail.com>
2022-05-16 22:17:25 -0700

    CityHospital

---
+++ cityhospital/src/App.js
@@ -0,0 +1,15 @@
+import Footer from "./Componets/Header/Footer/Footer";
+import Header from "./Componets/Header/Header";
+import Home from "./Container/Home/Home";
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <Home/>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
