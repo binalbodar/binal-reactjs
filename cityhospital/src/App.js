5ba0a7d03abf2513923eea772b5d027207da4b5a
binalbodar <binalbodar23@gmail.com>
2022-05-17 22:36:50 -0700

    18_5-cityhospitalproject

--- cityhospital/src/App.js
+++ cityhospital/src/App.js
@@ -1,3 +1,8 @@
+import { Route, Switch } from "react-router-dom";
+import About from "./Componets/About/About";
+import Contact from "./Componets/Contact/Contact";
+import Departments from "./Componets/Departments/Departments";
+import Doctors from "./Componets/Doctors/Doctors";
 import Footer from "./Componets/Header/Footer/Footer";
 import Header from "./Componets/Header/Header";
 import Home from "./Container/Home/Home";
@@ -6,7 +11,13 @@ function App() {
   return (
     <>
     <Header/>
-    <Home/>
+    <Switch>
+      <Route exact path={"/"} component={Home}/>
+      <Route exact path={"/departments"} component={Departments}/>
+      <Route exact path={"/doctors"} component={Doctors}/>
+      <Route exact path={"/about"} component={About}/>
+      <Route exact path={"/contact"} component={Contact}/>
+    </Switch>
     <Footer/>
     </>
   );
