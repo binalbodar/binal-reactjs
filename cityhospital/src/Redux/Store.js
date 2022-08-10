183958a7ac1655b4cffbe522d51eaa2250a3be93
binalbodar <binalbodar23@gmail.com>
2022-08-09 20:53:17 -0700

    10/8/22logaut

--- cityhospital/src/Redux/Store.js
+++ cityhospital/src/Redux/Store.js
@@ -9,6 +9,7 @@ import storage from 'redux-persist/lib/storage'
 const persistConfig = {
     key: 'root',
     storage,
+    whitelist: ["auth"]
   }
    
   const persistedReducer = persistReducer(persistConfig, rootReduser)
